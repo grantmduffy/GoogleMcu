85f884648211845b7646d02ceabafcb22f6725bb
Grant Duffy <grantd@buchanan-a.com>
2018-12-05 20:42:23 -0800

    Now blinks two leds

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import time
 import machine
 
-led = machine.Pin(16, machine.Pin.OUT)
+led1 = machine.Pin(2, machine.Pin.OUT)
+led2 = machine.Pin(16, machine.Pin.OUT)
+
 while True:
-    led.on()
+    led1.on()
+    led2.on()
     print('ON')
     time.sleep(0.5)
-    led.off()
+    led1.off()
+    led2.off()
     print('OFF')
     time.sleep(1.0)
